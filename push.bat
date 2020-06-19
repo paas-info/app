4f2d34c9dab588d10651e9be474ed1a6c6a69472
tom <tom@sapletta.com>
2020-06-19 13:24:41 +0200

    first version

--- push.bat
+++ push.bat
@@ -5,5 +5,5 @@ SetLocal EnableDelayedExpansion
 for /f %%i in ('dir /b /od Ticket\*.md') do set filename=%%i
 for /f "delims==" %%a in (Ticket\%filename%) do set message=%%a
 @ECHO ON
-changelog.bat && message.bat && git add . && git commit -m "%message%" && git push origin master
+changelog.bat && message.bat && git add . && git commit -m "%message%" && git push origin master && timeout /T 1 && remote_update.bat
 :: && docs.bat
