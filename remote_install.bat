c459b7b4ff949cc611139d0a5509f0e9a5217746
tom <tom@sapletta.com>
2020-06-19 13:31:29 +0200

    first version

---
+++ remote_install.bat
@@ -0,0 +1,5 @@
+cd F:\PycharmProjects\faas-ovh-api\
+py deploy.py app.paas.info status.sh
+py deploy.py app.paas.info remove.sh
+py deploy.py app.paas.info install.sh
+cd F:\WebstormProjects\paas-info-www
--- remote_update.bat
+++ remote_update.bat
@@ -1,6 +1,3 @@
 cd F:\PycharmProjects\faas-ovh-api\
-py deploy.py app.paas.info status.sh
-py deploy.py app.paas.info remove.sh
-py deploy.py app.paas.info install.sh
-::py deploy.py app.paas.info update.sh
+py deploy.py app.paas.info update.sh
 cd F:\WebstormProjects\paas-info-www
